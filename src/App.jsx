3c5e44ee4463c4c99718e94bafd25e3320d048ea
Henry Lok <143982077+HenryLok0@users.noreply.github.com>
2025-05-20 17:04:25 +0000

    update c to C

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
-import CarparkDetail from './components/carparkdetail';
+import CarparkDetail from './components/Carparkdetail';
 import { Routes, Route } from 'react-router-dom'; // 不要再 import Router
 
 function App() {
