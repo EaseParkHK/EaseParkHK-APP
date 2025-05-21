26e324073ce6dd2a599e0ed46d5038cff5a20720
Henry Lok <143982077+HenryLok0@users.noreply.github.com>
2025-05-21 11:06:24 +0000

    Updates

--- android/app/src/main/java/com/easeparkhk/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.easeparkhk.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.easeparkhk.app',
-  appName: 'EaseParkHK',
-  webDir: 'build'
-};
-
-export default config;
